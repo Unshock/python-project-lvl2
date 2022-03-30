f4875f5f29871d0c82b9d56caebe1d04c40ef327
Nedashkovsky <Nedashkovsky@ymail.com>
2022-03-30 22:50:02 +0300

    little mod of test

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -17,7 +17,14 @@ def make_json_file_path_2():
 
 def test_generate_diff(make_json_file_path_1, make_json_file_path_2):
     result = generate_diff(make_json_file_path_1, make_json_file_path_2)
-    diff = '{\n  - follow: false\n    host: hexlet.io\n  - proxy: 123.234.53.22\n  - timeout: 50\n  + timeout: 20\n  + verbose: true\n}'
+    diff = '{\n' \
+           '  - follow: false\n' \
+           '    host: hexlet.io\n' \
+           '  - proxy: 123.234.53.22\n' \
+           '  - timeout: 50\n' \
+           '  + timeout: 20\n' \
+           '  + verbose: true\n' \
+           '}'
 
     #result_diff = json.dumps(diff, indent=2)
     assert result == diff
