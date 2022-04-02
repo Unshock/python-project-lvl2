5aab2a80760c7dc873b7ec5a710ce66c124e2b61
Nedashkovsky <Nedashkovsky@ymail.com>
2022-04-02 21:30:19 +0300

    mod generator

--- gendiff/generator.py
+++ gendiff/generator.py
@@ -22,6 +22,17 @@ def make_normalization(function):
     return inner
 
 
+def make_element_dict(key, value, source=None, status=' '):
+    element_dict = {}
+
+    element_dict['key'] = key
+    element_dict['value'] = value
+    element_dict['source'] = source
+    element_dict['status'] = status
+
+    return element_dict
+
+
 def generate_diff(file_path_1, file_path_2):
     json_1 = json.load(open(file_path_1))
     json_2 = json.load(open(file_path_2))
@@ -39,42 +50,24 @@ def generate_diff(file_path_1, file_path_2):
 
 @make_normalization
 def make_checking_list(dict1, dict2):
-    dict1 = dict1
-    dict2 = dict2
+    #dict1 = dict1
+    #dict2 = dict2
 
     checking_list = []
 
     for key, value in dict1.items():
 
         if dict2.get(key) == value:
-            element_dict = {}
-
-            element_dict['key'] = key
-            element_dict['value'] = value
-            element_dict['source'] = 'dict_1'
-            element_dict['status'] = ' '
-
+            element_dict = make_element_dict(key, value, source='dict_1')
             checking_list.append(element_dict.copy())
 
         else:
-            element_dict = {}
-
-            element_dict['key'] = key
-            element_dict['value'] = value
-            element_dict['source'] = 'dict_1'
-            element_dict['status'] = '-'
-
+            element_dict = make_element_dict(key, value, source='dict_1', status='-')
             checking_list.append(element_dict.copy())
 
     for key, value in dict2.items():
         if dict1.get(key) != value:
-            element_dict = {}
-
-            element_dict['key'] = key
-            element_dict['value'] = value
-            element_dict['source'] = 'dict_2'
-            element_dict['status'] = '+'
-
+            element_dict = make_element_dict(key, value, source='dict_2', status='+')
             checking_list.append(element_dict.copy())
 
     return checking_list
